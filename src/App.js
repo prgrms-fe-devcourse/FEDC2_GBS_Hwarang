9680423a27ee475a4b53cea48a1e0e748a4b8b56
syoo970 <syu9710@gmail.com>
2022-06-08 14:19:39 +0900

    chore: prop-types 추가 및 eslint rule 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  return <div>dd</div>;
+  return (
+    <div>
+      <Router>{/* 여기에 react router dom code를 작성하시면 됩니다 */}</Router>
+    </div>
+  );
 }
 
 export default App;
