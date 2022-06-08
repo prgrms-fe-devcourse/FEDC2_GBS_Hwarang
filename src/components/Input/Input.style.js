981b1e5de3859d7989aa16e882d653283055bea1
YongSangYou <dydtkd113@gmail.com>
2022-06-08 18:05:27 +0900

    style: Input 컴포넌트의 스타일 코드를 작성했다.

--- src/components/Input/Input.style.js
+++ src/components/Input/Input.style.js
@@ -1,1 +1,28 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  display: ${({ block }) => (block ? "block" : "inline-block")};
+`;
+
+export const Label = styled.div`
+  display: block;
+  font-size: 12px;
+`;
+
+export const Search = styled.div`
+  display: flex;
+`;
+
+export const SearchInner = styled.div`
+  flex-grow: 1;
+  position: relative;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 100%;
+  padding: 4px 8px;
+  border: 1px solid ${({ invalid }) => (invalid ? "red" : "gray")};
+  border-radius: 4px;
+  box-sizing: border-box;
+`;
