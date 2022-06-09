c9ad2e959748aee4e8f16c708bd6b965a9d8bf7c
YongSangYou <dydtkd113@gmail.com>
2022-06-09 22:45:52 +0900

    Feat: Icon 컴포넌트 생성

---
+++ src/components/Icon/Icon.style.js
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+// eslint-disable-next-line import/prefer-default-export
+export const Icon = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
