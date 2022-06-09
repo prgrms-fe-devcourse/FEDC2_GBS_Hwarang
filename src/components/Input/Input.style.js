e583cd760f4c91f34fdf284445915c48e68cfacf
YongSangYou <dydtkd113@gmail.com>
2022-06-09 17:00:19 +0900

    style: Input 스타일을 변경했다.

--- src/components/Input/Input.style.js
+++ src/components/Input/Input.style.js
@@ -9,20 +9,20 @@ export const Label = styled.div`
   font-size: 12px;
 `;
 
-export const Search = styled.div`
-  display: flex;
-`;
-
-export const SearchInner = styled.div`
-  flex-grow: 1;
-  position: relative;
-`;
-
 export const Input = styled.input`
   width: 100%;
   height: 100%;
-  padding: 4px 8px;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  padding-left: 8px;
+  padding-right: 40px;
   border: 1px solid ${({ invalid }) => (invalid ? "red" : "gray")};
-  border-radius: 4px;
+  border-radius: 8px;
   box-sizing: border-box;
+  &:focus {
+    outline: none;
+  }
+  &::placeholder {
+    color: #ffc2c0;
+  }
 `;
