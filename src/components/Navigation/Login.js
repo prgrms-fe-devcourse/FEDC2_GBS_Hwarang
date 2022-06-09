47a618769dabedcda4827eab678a738cbfb5f442
syoo970 <syu9710@gmail.com>
2022-06-09 23:17:56 +0900

    feat: login, signup, logout 구현

--- src/components/Navigation/Login.js
+++ src/components/Navigation/Login.js
@@ -1,19 +1,30 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useSetRecoilState } from "recoil";
 import PropTypes from "prop-types";
 import * as Ns from "./Navigation.style";
+import { userLogin } from "../../api";
+import { loginProcess } from "../../recoil/authentication";
+import Divider from "../Divider";
 
 function Login({ handleLogin, onClose }) {
   const { register, handleSubmit } = useForm();
+  const setToken = useSetRecoilState(loginProcess);
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (data) => {
+    const { loginId, loginPassWord } = data;
+    const result = await userLogin(loginId, loginPassWord);
+
+    setToken(result.data.token);
+
     handleLogin(true);
     onClose();
   };
 
   return (
     <Ns.Form onSubmit={handleSubmit(onSubmit)}>
+      <Ns.Title>Login</Ns.Title>
+      <Divider />
       <input {...register("loginId", { required: true })} placeholder="ID" />
       <input
         {...register("loginPassWord", { required: true })}
