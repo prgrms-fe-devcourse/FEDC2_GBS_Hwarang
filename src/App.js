da4428e13df31cff43026252589d367aeb04acc4
Jungjjeong <sky990106@dgu.ac.kr>
2022-06-09 15:25:05 +0900

    Feat: Main Page Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import MainPage from "./pages";
 
 function App() {
   return (
     <div>
+      <MainPage />
       <Router>{/* 여기에 react router dom code를 작성하시면 됩니다 */}</Router>
     </div>
   );
