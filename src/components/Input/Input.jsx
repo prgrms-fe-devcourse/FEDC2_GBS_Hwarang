2a417212b4f920a99a2e9a9423b72ca659b4a1c8
YongSangYou <dydtkd113@gmail.com>
2022-06-09 17:00:41 +0900

    feat: 임시 컴포넌트를 추가했다.

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "@emotion/styled";
 import * as S from "./Input.style";
 
 const propTypes = {
@@ -12,19 +13,49 @@ const propTypes = {
   required: PropTypes.bool,
   disabled: PropTypes.bool,
   readOnly: PropTypes.bool,
+  borderColor: PropTypes.string,
+  fontColor: PropTypes.string,
+  icon: PropTypes.string,
 };
 
 const defaultProps = {
   width: 300,
   height: 50,
-  fontSize: 20,
+  fontSize: 14,
   label: "",
   block: false,
   invalid: false,
   required: false,
   disabled: false,
   readOnly: false,
+  borderColor: "#ec5e58",
+  fontColor: "#ffc2c0",
+  icon: "search",
 };
+
+const InputWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+/* 임시 컴포넌트 */
+const Actions = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  padding-right: 20px;
+`;
+/* 임시 컴포넌트 */
+const Icon = styled.span`
+  width: 20px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+`;
 
 const Input = ({
   width,
@@ -36,27 +67,37 @@ const Input = ({
   required,
   disabled,
   readOnly,
+  borderColor,
+  fontColor,
+  icon,
   ...props
 }) => {
-  const sizeStyle = {
+  const wrapperStyle = {
     width,
     height,
-  };
-  const fontSizeStyle = {
+    color: fontColor,
     fontSize,
   };
+  const inputStyle = {
+    border: `1px solid ${borderColor}`,
+  };
+
   return (
-    /* eslint-disable react/jsx-props-no-spreading */
-    <S.Wrapper style={sizeStyle} block={block}>
+    <S.Wrapper style={wrapperStyle} block={block}>
       <S.Label>{label}</S.Label>
-      <S.Input
-        invalid={invalid}
-        required={required}
-        disabled={disabled}
-        readOnly={readOnly}
-        style={fontSizeStyle}
-        {...props}
-      />
+      <InputWrapper>
+        <S.Input
+          invalid={invalid}
+          required={required}
+          disabled={disabled}
+          readOnly={readOnly}
+          style={inputStyle}
+          {...props}
+        />
+        <Actions>
+          <Icon className="material-symbols-outlined">{icon}</Icon>
+        </Actions>
+      </InputWrapper>
     </S.Wrapper>
   );
 };
