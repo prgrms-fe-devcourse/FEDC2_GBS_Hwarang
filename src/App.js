bb5e0d156d3d1b74ffd51f86b5d387e39d9368f5
aurora32s <aurora32s@naver.com>
2022-06-10 14:09:01 +0900

    github and slack test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+/** @format */
+
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import MainPage from "./pages";
 
 function App() {
   return (
+    // test
     <div>
       <MainPage />
       <Router>{/* 여기에 react router dom code를 작성하시면 됩니다 */}</Router>
