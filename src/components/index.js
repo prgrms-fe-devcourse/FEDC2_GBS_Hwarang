e8686a286dcd420c517bed95ba9fff92c35768d2
aurora32s <aurora32s@naver.com>
2022-06-10 22:24:01 +0900

    [fix]: rebase conflict

--- src/components/index.js
+++ src/components/index.js
@@ -6,8 +6,5 @@ export { default as IconGroup } from "./IconGroup";
 export { default as Image } from "./Image";
 export { default as Input } from "./Input";
 export { default as Text } from "./Text";
-<<<<<<< HEAD
 export { default as ToggleButton } from "./ToggleButton";
-=======
 export { default as Popup } from "./Popup";
->>>>>>> f2b2531 ([feat]: Popup component 기본 코드 추가)
