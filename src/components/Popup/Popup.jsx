0feb65b068fcb10a93cdfcfc5ab0b33da0f249e3
aurora32s <aurora32s@naver.com>
2022-06-11 23:27:50 +0900

    [feat]: sidebar 기능 구현(상,하,좌,우 방향 가능) 및 dim 선택적 추가 가능

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,23 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Wrapper from "./Popup.style";
+import { PopupContainer, PopupWrapper, PopupCloseBtn } from "./Popup.style";
 
 const propTypes = {
+  children: PropTypes.node,
   type: PropTypes.string, // popup type (left, right, top, bottom)
-  size: PropTypes.arrayOf([PropTypes.number, PropTypes.string]), // popup size (left/right일 때는 width, top/bottom일 때는 height)
   show: PropTypes.bool, // popup show/hide flag
+  size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  dim: PropTypes.bool, // background dim show/hide flag
+  onClose: PropTypes.func, // pop up close event
 };
 const defaultProps = {
+  children: undefined,
   type: "left",
-  size: 20,
   show: false,
+  size: "50%",
+  dim: false,
+  onClose: undefined,
 };
 
-const Popup = ({ type, size, show }) => {
+const Popup = ({ children, show, type, size, dim, onClose }) => {
+  const handleOnClose = () => {
+    if (onClose) onClose();
+  };
+
   return (
-    <div>
-      <Wrapper type={type} size={size} className={show ? "popup-show" : ""} />
-    </div>
+    <PopupWrapper className={show ? "popup__wrap_show" : ""} type={type}>
+      {dim && <div id="dim" />}
+      <PopupContainer className="popup__container" type={type} size={size}>
+        {children}
+        <PopupCloseBtn
+          type="button"
+          className="popup__close_btn"
+          onClick={handleOnClose}
+        >
+          X
+        </PopupCloseBtn>
+      </PopupContainer>
+    </PopupWrapper>
   );
 };
 
