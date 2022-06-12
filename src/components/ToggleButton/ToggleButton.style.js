9171942f32fbe1712fe84e133f566697f40a2f9e
Jungjjeong <sky990106@dgu.ac.kr>
2022-06-12 20:48:03 +0900

    Style: ToggleButton Style 추가

---
+++ src/components/ToggleButton/ToggleButton.style.js
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+const Wrapper = styled.button`
+  display: flex;
+  align-items: center;
+  border: none;
+  background-color: rgba(0, 0, 0, 0);
+`;
+
+const TextWrapper = styled.div`
+  flex-shrink: 1;
+`;
+
+export default { Wrapper, TextWrapper };
