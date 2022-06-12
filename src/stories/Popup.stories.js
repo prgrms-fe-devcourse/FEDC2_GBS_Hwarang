c1faa6906f00196bab5c1813ba998cbe5384bc15
aurora32s <aurora32s@naver.com>
2022-06-12 14:03:00 +0900

    [fix] rebase conflict - Button onClick prop

--- src/stories/Popup.stories.js
+++ src/stories/Popup.stories.js
@@ -11,31 +11,61 @@ export const LeftPopup = () => {
   return (
     <div>
       <Button onClick={() => setShow((pre) => !pre)}>Click</Button>
-      <Popup size="30px" show={show} />
+      <Popup show={show} size={300} onClose={() => setShow((pre) => !pre)}>
+        Hello World
+      </Popup>
     </div>
   );
 };
 
 export const RightPopup = () => {
+  const [show, setShow] = useState(false);
   return (
     <div>
-      <Popup type="right" />
+      <Button onClick={() => setShow((pre) => !pre)}>Click</Button>
+      <Popup
+        show={show}
+        size="20%"
+        type="right"
+        dim
+        onClose={() => setShow((pre) => !pre)}
+      >
+        Hello World
+      </Popup>
     </div>
   );
 };
 
 export const TopPopup = () => {
+  const [show, setShow] = useState(false);
   return (
     <div>
-      <Popup type="top" />
+      <Button onClick={() => setShow((pre) => !pre)}>Click</Button>
+      <Popup
+        show={show}
+        size="20%"
+        type="top"
+        onClose={() => setShow((pre) => !pre)}
+      >
+        Hello World
+      </Popup>
     </div>
   );
 };
 
 export const BottomPopup = () => {
+  const [show, setShow] = useState(false);
   return (
     <div>
-      <Popup type="bottom" />
+      <Button onClick={() => setShow((pre) => !pre)}>Click</Button>
+      <Popup
+        show={show}
+        size="20%"
+        type="bottom"
+        onClose={() => setShow((pre) => !pre)}
+      >
+        Hello World
+      </Popup>
     </div>
   );
 };
