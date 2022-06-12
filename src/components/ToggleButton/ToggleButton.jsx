6c899b7e1f08236ec8d56d25016b486776a922ed
Jungjjeong <sky990106@dgu.ac.kr>
2022-06-12 20:43:26 +0900

    Feat: ToggleButton logo, text prop 추가

--- src/components/ToggleButton/ToggleButton.jsx
+++ src/components/ToggleButton/ToggleButton.jsx
@@ -1,35 +1,115 @@
 import styled from "@emotion/styled";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { Icon, Text } from "components";
+import { Text } from "components";
+import Common from "styles/common";
 
 const propTypes = {
-  children: PropTypes.number,
-  iconName: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.string]).isRequired,
+  replaceChildren: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
+  text: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+    PropTypes.node,
+  ]),
+  textColor: PropTypes.string,
   textSize: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  justify: PropTypes.string,
+  strong: PropTypes.bool,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  gap: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 };
 
 const defaultProps = {
-  children: 10,
-  iconName: "favorite",
-  textSize: "$b3",
-  justify: "end",
+  replaceChildren: undefined,
+  text: "undefined",
+  textColor: Common.colors.black01,
+  textSize: "$c2",
+  strong: false,
+  disabled: true,
+  onClick: undefined,
+  gap: 10,
 };
 
-const Wrapper = styled.div`
+/* 내부 children Required Props
+   1. logo
+    아이콘 사용 시 : 아이콘 이름, width, height
+    이미지 사용 시 : 이미지 src, width, height
+    아바타 사용 시 : 이미지 src, size(width, height)
+      ---> children으로 node를 받고 크기만 설정해주도록.
+
+  2. Text
+    일반 줄글 사용 시 : color, size, strong
+    TextNode 사용 시 : 해당 노드에 prop 담겨서 들어옴
+      ---> propTypes : string인 경우, Text에 넣어 줌
+      ---> propTypes : node인 경우, 그대로 렌더링
+
+  + readonly prop (단순 조회) <bool>
+  + onCLick prop (클릭 시 이벤트)
+*/
+
+const Wrapper = styled.button`
   display: flex;
   align-items: center;
+  border: none;
+  background-color: rgba(0, 0, 0, 0);
 `;
 
-const ToggleButton = ({ children, iconName, textSize, justify }) => {
-  const buttonStyle = {
-    justifyContent: justify,
+const LogoWrapper = styled.div``;
+
+const TextWrapper = styled.div`
+  flex-shrink: 1;
+`;
+
+const ToggleButton = ({
+  children,
+  replaceChildren,
+  onClick,
+  text,
+  textColor,
+  textSize,
+  strong,
+  disabled,
+  gap,
+}) => {
+  const [clicked, setClicked] = useState(false);
+  const [currentChild, setCurrentChild] = useState(children);
+
+  const WrapperStyle = {
+    gap,
   };
+
+  const textStyle = {
+    color: textColor,
+    fontWeight: strong ? "bold" : undefined,
+  };
+
+  const handleClicked = () => {
+    onClick();
+    setClicked(!clicked);
+  };
+
+  useEffect(() => {
+    if (!replaceChildren) return;
+
+    if (!clicked) {
+      setCurrentChild(children);
+      return;
+    }
+
+    setCurrentChild(replaceChildren);
+  }, [clicked]);
+
   return (
-    <Wrapper style={buttonStyle}>
-      <Icon name={iconName} width={30} height={30} />
-      <Text size={textSize}>{children}</Text>
+    <Wrapper onClick={handleClicked} disabled={disabled} style={WrapperStyle}>
+      <LogoWrapper>{currentChild}</LogoWrapper>
+      <TextWrapper style={textStyle}>
+        {typeof text === "string" || typeof text === "number" ? (
+          <Text size={textSize}>{text}</Text>
+        ) : (
+          text
+        )}
+      </TextWrapper>
     </Wrapper>
   );
 };
