515275b35cf88a716bbe2f4adb49819790090808
aurora32s <aurora32s@naver.com>
2022-06-13 18:21:23 +0900

    [feat]: post 관련 repository 구현 및 test 용 mock 데이터 연동 완료

--- src/repository/postRepository.js
+++ src/repository/postRepository.js
@@ -1,3 +1,27 @@
 /**
  * Post 관련 repository
  */
+
+import { getChannels, getPosts } from "api/post-api";
+
+/**
+ * 메인 페이지에서 사용되는 인기 순, 최신 순 게시글 요청
+ */
+export default getAllPost = async () => {
+  try {
+    const channelResponse = await getChannels();
+    if (channelResponse.data) {
+      const postByChannel = await Promise.all(
+        // eslint-disable-next-line no-underscore-dangle
+        channelResponse.data.map((channel) => getPosts(channel._id))
+      );
+      const posts = channelResponse.data.map((channel, index) => ({
+        ...channel,
+        posts: postByChannel[index],
+      }));
+      return posts;
+    }
+  } catch (exception) {
+    console.error(exception);
+  }
+};
