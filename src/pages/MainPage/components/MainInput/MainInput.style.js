963c2df483df17e871c3d35e0dfd1ef463e3cbec
aurora32s <aurora32s@naver.com>
2022-06-13 23:20:52 +0900

    [feat]: main 화면 검색 element 구현

---
+++ src/pages/MainPage/components/MainInput/MainInput.style.js
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export default MainInput = styled.div`
+  position: absolute;
+  top: 430px;
+  right: calc(50% - 400px);
+  width: 800px;
+  height: 75px;
+  border-radius: 20px;
+`;
