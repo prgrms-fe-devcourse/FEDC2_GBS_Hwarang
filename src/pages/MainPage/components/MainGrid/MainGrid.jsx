51933b2ccb29ae570c999630c7f0d0afc1ca377e
aurora32s <aurora32s@naver.com>
2022-06-14 15:20:19 +0900

    [fix]:rebase conflict 수정

--- src/pages/MainPage/components/MainGrid/MainGrid.jsx
+++ src/pages/MainPage/components/MainGrid/MainGrid.jsx
@@ -15,6 +15,7 @@ const propTypes = {
 
 const MainGrid = ({ data, mainTitle }) => {
   const { FluxRow, FluxCol } = Flux;
+  console.log(data);
   return (
     <S.MainGridWrapper>
       <S.TextWrapper>
