cf4a8317f36de0a316ceb437364d3b2eecb879a6
syoo970 <syu9710@gmail.com>
2022-06-14 16:08:19 +0900

    feat: 이미지 업로드 기능 추가

---
+++ src/stories/ImageButton.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ImageButton from "pages/UserPage/components/ImageButton";
+
+export default {
+  title: "Component/ImageButton",
+  component: ImageButton,
+};
+
+export const ImageButtonExample = () => <ImageButton />;
