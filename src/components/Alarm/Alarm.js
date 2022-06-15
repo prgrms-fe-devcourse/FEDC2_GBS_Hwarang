fdb862ab65cb321bdd58e17aef46c39b624aaae2
aurora32s <aurora32s@naver.com>
2022-06-15 15:51:18 +0900

    [feat]: 알림 타입별 element 구성 완료

--- src/components/Alarm/Alarm.js
+++ src/components/Alarm/Alarm.js
@@ -4,6 +4,8 @@
 import Avatar from "components/Avatar";
 import Dot from "components/Dot";
 import Text from "components/Text";
+import Image from "components/Image";
+import Divider from "components/Divider";
 import React from "react";
 import * as S from "./Alarm.style";
 
@@ -16,7 +18,60 @@ const Alarm = () => {
         <Text size="$c1" strong>
           알림 왔슈📌
         </Text>
-        hello world
+        <ul style={{ listStyle: "none", padding: 0 }}>
+          <li style={{ display: "flex" }}>
+            <div>
+              <Text strong size="$c1" style={{ display: "inline-block" }}>
+                신승연
+              </Text>
+              님이
+              <Text strong size="$c1" style={{ display: "inline-block" }}>
+                {`"`}멋저용{`"`}
+              </Text>
+              댓글을 달았습니다.
+            </div>
+            <Image
+              src="https://via.placeholder.com/300"
+              width="50px"
+              height="auto"
+              mode="contain"
+              style={{ marginLeft: 10 }}
+            />
+          </li>
+          <Divider style={{ opacity: 0.1 }} size={15} />
+          <li style={{ display: "flex" }}>
+            <div>
+              <Text strong size="$c1" style={{ display: "inline-block" }}>
+                유승범
+              </Text>
+              님이
+              <Text strong size="$c1" style={{ display: "inline-block" }}>
+                진연주
+              </Text>
+              님을 구독했습니다.
+            </div>
+          </li>
+          <Divider style={{ opacity: 0.1 }} size={15} />
+          <li style={{ display: "flex" }}>
+            <div>
+              <Text strong size="$c1" style={{ display: "inline-block" }}>
+                유용상
+              </Text>
+              님이
+              <Text strong size="$c1" style={{ display: "inline-block" }}>
+                {`"`}멋진 한국의 한옥마....{`" `}
+              </Text>
+              일정을 좋아합니다.
+            </div>
+            <Image
+              src="https://via.placeholder.com/300"
+              width="50px"
+              height="auto"
+              mode="contain"
+              style={{ marginLeft: 10 }}
+            />
+          </li>
+        </ul>
       </S.AlarmWrapper>
     </div>
   );
