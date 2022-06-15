02f1fddde4a41ed9cd76e4b8398c27ea952b68e9
dustmddus <dustmddus5@gmail.com>
2022-06-15 23:47:49 +0900

    pick

--- src/pages/PostListPage/index.js
+++ src/pages/PostListPage/index.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { Footer, PostList, PostListFilter } from "components";
-=======
-import { Footer, PostListFilter } from "components";
->>>>>>> 3b895a97ebe96580eaf9aaa08432a13a9c61e8c1
 import React from "react";
 import DummyData from "pages/MainPage/dummyData";
 import TaskProvider from "contexts/TaskProvider";
@@ -10,32 +6,15 @@ import S from "./PostList.style";
 
 const PostListPage = () => {
   return (
-    <div>
-<<<<<<< HEAD
-      <TaskProvider>
-        <S.Header /* Header */>
-          <PostListFilter />
-        </S.Header>
-        <S.Section /* PostList 렌더링 */>
-          <PostList data={DummyData} listTitle="검색 결과" />
-        </S.Section>
-        <Footer />
-      </TaskProvider>
-=======
+    <TaskProvider>
       <S.Header /* Header */>
         <PostListFilter />
-        <buttonSection>
-          {/* 검색 기준 */}
-          <searchOptionsSelected /* 고른 options */ />
-          <searchOptions /* 고를 수 있는 options */ />
-        </buttonSection>
       </S.Header>
       <S.Section /* PostList 렌더링 */>
-        <postList /* 검색 결과에 따른 PostList */ />
+        <PostList data={DummyData} listTitle="검색 결과" />
       </S.Section>
       <Footer />
->>>>>>> 3b895a97ebe96580eaf9aaa08432a13a9c61e8c1
-    </div>
+    </TaskProvider>
   );
 };
 
