b3cdf0e7ce1cf47a21d08d2eddc47a8226232743
dustmddus <dustmddus5@gmail.com>
2022-06-16 17:56:07 +0900

    fix : 충돌 부분 수정

--- src/components/PostListFilter/PostListFilter.style.js
+++ src/components/PostListFilter/PostListFilter.style.js
@@ -15,10 +15,14 @@ export const filterContainer = styled.div`
   grid-template-rows: 1fr 3fr;
   width: 700px;
 <<<<<<< HEAD
+<<<<<<< HEAD
   height: 200px;
   margin-top: 50px;
 =======
   height: 250px;
+=======
+  height: 200px;
+>>>>>>> 3b895a9 (style:filter 컴포넌트 스타일 작업)
   margin-top: 80px;
   border: 1px solid #ffc2c0;
 `;
@@ -28,6 +32,7 @@ export const searchSelected = styled.div`
 >>>>>>> 89d238a (feat:postListFilter 컴포넌트 생성)
 `;
 
+<<<<<<< HEAD
 export const searchSelected = styled.div`
   background-color: #f5f5f5;
   padding: 10px;
@@ -36,6 +41,8 @@ export const searchSelected = styled.div`
   overflow-x: auto; */
 `;
 
+=======
+>>>>>>> 3b895a9 (style:filter 컴포넌트 스타일 작업)
 export const searchOptions = styled.div`
   display: grid;
   grid-template-rows: 1fr 1fr;
@@ -61,6 +68,7 @@ export const styledButton = styled.button`
   border-radius: 18px;
   padding: 8px;
   margin-right: 15px;
+<<<<<<< HEAD
   display: inline-block;
   cursor: pointer;
   &:hover {
@@ -74,4 +82,8 @@ export const styledButton = styled.button`
   }
 =======
 >>>>>>> 6e784ff (feat:같은 검색어 추가 방지 코드 추가)
+=======
+  background-color: white;
+  display: inline-block;
+>>>>>>> 3b895a9 (style:filter 컴포넌트 스타일 작업)
 `;
