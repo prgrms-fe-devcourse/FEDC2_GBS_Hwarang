f52808aa15c36e73edf7f411a20315e5c697acb1
Jungjjeong <sky990106@dgu.ac.kr>
2022-06-16 16:47:39 +0900

    Fix: git Conflict 해결

--- src/pages/PostListPage/index.js
+++ src/pages/PostListPage/index.js
@@ -2,8 +2,8 @@ import { PostList, PostListFilter } from "components";
 import React, { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { allPost } from "recoil/post";
-import S from "./PostListPage.style";
 import TaskProvider from "contexts/TaskProvider";
+import S from "./PostListPage.style";
 import ScrollTopButton from "./components/ScrollTopButton";
 
 const PostListPage = () => {
@@ -32,16 +32,16 @@ const PostListPage = () => {
     <TaskProvider>
       <S.PageWrapper>
         <S.HeaderWrapper>
-          <S.Header /* Header */ className={folded ? "fold__header" : "">
+          <S.Header /* Header */ className={folded ? "fold__header" : ""}>
             <PostListFilter />
           </S.Header>
         </S.HeaderWrapper>
-      <S.Section /* PostList 렌더링 */>
-        <PostList data={data} listTitle="검색 결과" />
-      </S.Section>
-      <ScrollTopButton />
-    </S.PageWrapper>
-   </TaskProvider>
+        <S.Section /* PostList 렌더링 */>
+          <PostList data={data} listTitle="검색 결과" />
+        </S.Section>
+        <ScrollTopButton />
+      </S.PageWrapper>
+    </TaskProvider>
   );
 };
 
