bd4310f07b563088b117681c0b92f7f35edca024
aurora32s <aurora32s@naver.com>
2022-06-17 17:07:31 +0900

    [fix]: rebase conflict 수정

---
+++ src/components/Dot/Dot.style.js
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export default DotItem = styled.div`
+  width: 10px;
+  height: 10px;
+  background-color: #53bf9d;
+  border-radius: 50%;
+  border: none;
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: 14px;
+`;
