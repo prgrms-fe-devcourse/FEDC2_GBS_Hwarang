e296c7b6ba0f1531774178bb3e623592480360c6
dustmddus <dustmddus5@gmail.com>
2022-06-17 16:06:38 +0900

    pickfeat:필터링 컴포넌트 분리 생성

--- src/pages/PostListPage/components/FilteredResult/FilteredResult.jsx
+++ src/pages/PostListPage/components/FilteredResult/FilteredResult.jsx
@@ -1,5 +1,6 @@
 import { useTasks } from "contexts/TaskProvider";
 
+<<<<<<< HEAD
 const FilteredResult = (data) => {
   const { tasks, channel, standard } = useTasks();
 
@@ -29,6 +30,37 @@ const FilteredResult = (data) => {
   result = standardFilter(result);
   result = titleFilter(result);
   result = channelFilter(result);
+=======
+const { tasks, channel, standard } = useTasks();
+
+const titleFilter = (data) => {
+  if (tasks.length !== 0) {
+    const titleSet = tasks.map((item) => item.title);
+    return data.filter((item) => titleSet.includes(item.title));
+  }
+  return data;
+};
+
+const channelFilter = (data) => {
+  if (channel.length !== 0) {
+    return data.filter((item) => item.channel === channel);
+  }
+  return data;
+};
+
+const standardFilter = (data) => {
+  if (standard.length !== 0) {
+    // 각 기준 별로 리코일 요청하면 될듯
+    return data;
+  }
+};
+
+const FilteredResult = (data) => {
+  let result = data;
+  result = titleFilter(result);
+  result = channelFilter(result);
+  result = standardFilter(result);
+>>>>>>> b06a432 (feat:필터링 컴포넌트 분리 생성)
 
   return result;
 };
