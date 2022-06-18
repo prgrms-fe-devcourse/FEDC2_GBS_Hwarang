df39f4ff89f0694857f6563ca7debd132567be09
aurora32s <aurora32s@naver.com>
2022-06-18 15:12:57 +0900

    [fix]: rebase conflict 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 export { default as MainPage } from "./MainPage";
 export { default as PostListPage } from "./PostListPage";
 export { default as UserPage } from "./UserPage";
+=======
+import MainPage from "./MainPage/MainPage";
+
+export default MainPage;
+>>>>>>> 6b4a437 (Feat: MainGrid, MainGridItem Component 생성)
