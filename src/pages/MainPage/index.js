b4ac6eec5b26cedbeca1de4158714d7c82920fa5
aurora32s <aurora32s@naver.com>
2022-06-18 19:03:40 +0900

    [fix]: rebase conflict 수정

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,58 +1,38 @@
 import React from "react";
-import { Button, Image, ImageSlider } from "components";
-import { useRecoilValue } from "recoil";
-import { mainPost } from "recoil/post";
-import Common from "styles/common";
 import S from "./MainPage.style";
-import { MainGrid, MainInput, ImageData } from "./components";
+import Image from "../../components/Image";
+import MainGrid from "./components/MainGrid";
 
 const MainPage = () => {
-  const { popularPost, latestPost } = useRecoilValue(mainPost);
-
   return (
     <div>
+      <S.NavBar>
+        {/* Navigation Bar */}
+        가봤슈 Navigation Bar
+      </S.NavBar>
       <S.Header>
-        <S.HeaderCarousel>
-          <ImageSlider width="100%" height="auto">
-            {ImageData}
-          </ImageSlider>
-        </S.HeaderCarousel>
+        <S.HeaderCarousel>{/* Main Carousel */}</S.HeaderCarousel>
         <S.HeaderText>여기 가봤슈?</S.HeaderText>
-        <MainInput />
+        <S.HeaderInput /* absolute Position Input */ />
       </S.Header>
       <S.Section>
-        <S.SectionWrapper>
-          <MainGrid
-            data={popularPost}
-            mainTitle="가봤슈 사용자들의 최고 인기 여행지"
-          />
-          <Button color="white" type="button">
-            <S.LinkButton to="/travel-destination" style={{ color: "white" }}>
-              더보기
-            </S.LinkButton>
-          </Button>
-        </S.SectionWrapper>
+        <div>
+          <h1>가봤슈 사용자들의 최고 인기 여행지</h1>
+          <MainGrid />
+          <S.Button type="button">더보기</S.Button>
+        </div>
         <Image
-          /* Marketing Image */
-          src="https://mygbs.s3.ap-northeast-2.amazonaws.com/marketing/marketing_Image+(1).png"
+          /* Marketing Image */ src="https://user-images.githubusercontent.com/72294509/172774786-3db0d345-a7e9-4f30-99bc-adf56a82d87d.png"
           width="100%"
           height="100%"
         />
-        <S.SectionWrapper>
-          <MainGrid
-            data={latestPost}
-            mainTitle="가봤슈 사용자들의 최근 여행지"
-          />
-          <Button color="$main" backgroundColor="$white" type="button" border>
-            <S.LinkButton
-              to="/travel-destination"
-              style={{ color: Common.colors.main }}
-            >
-              더보기
-            </S.LinkButton>
-          </Button>
-        </S.SectionWrapper>
+        <div>
+          <h1>가봤슈에 올라온 최근 여행지</h1>
+          <MainGrid />
+          <S.Button type="button">더보기</S.Button>
+        </div>
       </S.Section>
+      <S.Footer>Footer</S.Footer>
     </div>
   );
 };
