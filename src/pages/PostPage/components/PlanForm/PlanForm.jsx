b3ba54104cab544818839885ea4ecffdb99f1663
YongSangYou <dydtkd113@gmail.com>
2022-06-18 13:52:43 +0900

    Feat: 여행일정 컴포넌트 생성

---
+++ src/pages/PostPage/components/PlanForm/PlanForm.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Image, Input, Icon, ImageUploader } from "components";
+import Common from "styles/common";
+import useHover from "hooks/useHover";
+import S from "./PlanForm.style";
+import Textarea from "../Textarea";
+
+const propTypes = {
+  plan: PropTypes.instanceOf(Object),
+  type: PropTypes.string,
+  onChangeHandler: PropTypes.func,
+  removePlan: PropTypes.func,
+  onImageChange: PropTypes.func,
+  removePlanImage: PropTypes.func,
+};
+
+const defaultProps = {
+  plan: {},
+  type: null,
+  onChangeHandler: () => {},
+  removePlan: () => {},
+  onImageChange: () => {},
+  removePlanImage: () => {},
+};
+
+const PlanForm = ({
+  plan,
+  // eslint-disable-next-line no-unused-vars
+  type,
+  onChangeHandler,
+  removePlan,
+  onImageChange,
+  removePlanImage,
+}) => {
+  const { _id, image, title, content } = plan;
+  const [isHovering, handleMouseEnter, handleMouseLeave] = useHover({
+    content: false,
+    image: false,
+  });
+
+  return (
+    <S.Content
+      key={_id}
+      name="content"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <S.Dot isHovering={isHovering.content}>
+        {isHovering.content && (
+          <Icon name="close" onClick={() => removePlan(_id)} />
+        )}
+      </S.Dot>
+      <S.ImageContainer
+        name="image"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <S.ImageWrapper>
+          <Image
+            width="100%"
+            height="100%"
+            src={image}
+            styled={{ borderRadius: 6 }}
+          />
+        </S.ImageWrapper>
+        {/* Todo: 파일 불러오기 */}
+        <S.ButtonWrapper isHovering={isHovering.image}>
+          <ImageUploader
+            id={_id}
+            onChange={onImageChange}
+            btnText="사진 업로드"
+            useButton
+          />
+        </S.ButtonWrapper>
+        <S.IconWrapper isHovering={isHovering.image}>
+          <Icon name="close" onClick={() => removePlanImage(_id)} />
+        </S.IconWrapper>
+      </S.ImageContainer>
+      <S.Info>
+        <Input
+          placeholder="여행지 이름을 입력해 주세요."
+          defaultValue={title}
+          onChange={(e) => onChangeHandler(_id, "title", e.target.value)}
+          useIcon={false}
+          maxLength="30"
+          width="100%"
+          style={{
+            backgroundColor: "transparent",
+            border: "none",
+            fontSize: `${Common.fontSize.b1}`,
+            fontWeight: 600,
+            padding: 0,
+          }}
+        />
+        <Textarea
+          placeholder="해당 일정에 대해 설명해 주세요!"
+          defaultValue={content}
+          onChange={(e) => onChangeHandler(_id, "content", e.target.value)}
+          maxLength="120"
+          height={100}
+          backgroundColor="transparent"
+        />
+      </S.Info>
+    </S.Content>
+  );
+};
+
+PlanForm.propTypes = propTypes;
+PlanForm.defaultProps = defaultProps;
+
+export default PlanForm;
---
+++ src/pages/PostPage/components/PlanForm/PlanForm.style.js
@@ -0,0 +1,80 @@
+import styled from "@emotion/styled";
+import Common from "styles/common";
+
+const Content = styled.li`
+  display: flex;
+  padding: 24px 0;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const Dot = styled.div`
+  align-self: center;
+  width: 20px;
+  height: 20px;
+  margin-left: 100px;
+  margin-right: 80px;
+  border: 1px solid ${Common.colors.main};
+  border-radius: 50%;
+  background-color: ${({ isHovering }) =>
+    isHovering ? `${Common.colors.white}` : `${Common.colors.main}`};
+  color: ${Common.colors.main};
+  z-index: 1;
+  cursor: pointer;
+`;
+
+const ImageContainer = styled.div`
+  position: relative;
+`;
+
+const ImageWrapper = styled.div`
+  width: 280px;
+  height: 180px;
+  border-radius: 20px;
+  overflow: hidden;
+`;
+
+const ButtonWrapper = styled.div`
+  display: ${({ isHovering }) => (isHovering ? "block" : "none")};
+  position: absolute;
+  top: 40%;
+  left: 0;
+  right: 0;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: -10px;
+  right: -20px;
+  display: ${({ isHovering }) => (isHovering ? "flex" : "none")};
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  background-color: white;
+  border-radius: 50%;
+  border: 1px solid ${Common.colors.gray03};
+  cursor: pointer;
+`;
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  margin-left: 16px;
+  margin-top: 16px;
+  input::placeholder {
+    color: ${Common.colors.gray04};
+  }
+`;
+
+export default {
+  Content,
+  Dot,
+  ImageContainer,
+  ImageWrapper,
+  ButtonWrapper,
+  IconWrapper,
+  Info,
+};
