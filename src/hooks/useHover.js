eefd973d3404b84636b6262cd59af6c65fda4040
YongSangYou <dydtkd113@gmail.com>
2022-06-19 16:31:32 +0900

    Refactor: detail 페이지에서는 사용하지 않음

--- src/hooks/useHover.js
+++ src/hooks/useHover.js
@@ -1,14 +1,20 @@
 import { useState } from "react";
 
-const useHover = (target) => {
+const useHover = (target, type = "") => {
   const [isHovering, setIsHovering] = useState({ ...target });
 
   const handleMouseEnter = (e) => {
+    if (type === "detail") {
+      return;
+    }
     const name = e.currentTarget.getAttribute("name");
     setIsHovering({ ...isHovering, [name]: true });
   };
 
   const handleMouseLeave = (e) => {
+    if (type === "detail") {
+      return;
+    }
     const name = e.currentTarget.getAttribute("name");
     setIsHovering({ ...isHovering, [name]: false });
   };
