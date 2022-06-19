a47ca373825af1e68521d43dc22c2eb9c4248110
YongSangYou <dydtkd113@gmail.com>
2022-06-20 00:29:44 +0900

    Refactor: try catch 추가

--- src/utils/S3ImageHandler.js
+++ src/utils/S3ImageHandler.js
@@ -13,8 +13,13 @@ export const uploadImageToS3 = async (file, directory = "") => {
 
   const s3 = new ReactS3Client(config);
 
-  const res = await s3.uploadFile(file, file.name);
-  return res;
+  try {
+    const res = await s3.uploadFile(file, file.name);
+    return res;
+  } catch (exception) {
+    // eslint-disable-next-line no-console
+    console.log(exception);
+  }
 };
 
 export const deleteImageToS3 = async (file, directory = "") => {
@@ -39,13 +44,19 @@ export const deleteImageToS3 = async (file, directory = "") => {
 export const listFiles = async () => {
   const config = {
     bucketName: "mygbs",
-    dirName: "test",
+    dirName: "",
     region: "ap-northeast-2",
     accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
     secretAccessKey: process.env.REACT_APP_AWS_SECRET_KEY,
   };
 
   const s3 = new ReactS3Client(config);
-  const fileList = await s3.listFiles();
-  console.log(fileList);
+  try {
+    const fileList = await s3.listFiles();
+    // eslint-disable-next-line no-console
+    console.log(fileList);
+  } catch (exception) {
+    // eslint-disable-next-line no-console
+    console.log(exception);
+  }
 };
