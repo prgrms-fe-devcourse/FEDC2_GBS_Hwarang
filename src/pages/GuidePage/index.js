6440295f61888c1af00601acce52ecc26258f177
Jungjjeong <sky990106@dgu.ac.kr>
2022-06-22 14:21:51 +0900

    Feat: GuidePage image src 변경

--- src/pages/GuidePage/index.js
+++ src/pages/GuidePage/index.js
@@ -5,17 +5,29 @@ import React from "react";
 const Wrapper = styled.div`
   box-sizing: border-box;
   width: 100%;
-  height: auto;
-  padding: 50px 0 0 0;
+  height: 800px;
+  margin: 50px 0 0 0;
+  animation: fadeIn 1s ease-in-out;
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+      transform: translateY(20px);
+    }
+    to {
+      opacity: 3;
+      transform: none;
+    }
+  }
 `;
 
 const GuidePage = () => {
   return (
     <Wrapper>
       <Image
-        src="https://user-images.githubusercontent.com/72294509/174862615-e95173dd-a6dd-4502-a4ca-1ce77a0b5059.png"
-        width="50%"
-        height="auto"
+        src="https://mygbs.s3.ap-northeast-2.amazonaws.com/gbs-logo2.png"
+        width="auto"
+        height="800px"
         style={{ margin: "0 auto" }}
       />
     </Wrapper>
